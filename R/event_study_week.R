209fa8acec270c6923de4f6421bed5aaa28957c6
miketopper123 <miketopper123@gmail.com>
2021-04-08 00:05:10 -0700

    redid the event study function

--- R/event_study_week.R
+++ R/event_study_week.R
@@ -11,16 +11,16 @@
 #'
 #' @param data an object/dataframe/tibble.
 #' @param window_length the column in the object that contains the  end date of the first moratorium.
-#'
+#' @param reference_week a number for which year you want to be the reference year. It will be paste0("treatment_minus_", reference_year)
 #'
 #' @examples
-#' event_study_month(monthly_crime, 2014, 2019, 4)
+#' event_study_week(weekly_panel,  4, 1)
 #'
 #' @export event_study_week
 #'
 
 
-event_study_week <- function(data, window_length) {
+event_study_week <- function(data, window_length, reference_week) {
   window <- c(1:window_length)
   weekly_crime <- data %>%
     mutate(across(c(closure_1_end, closure_2_end), ~ as.character(.))) %>%
@@ -28,74 +28,69 @@ event_study_week <- function(data, window_length) {
                                   closure_2_end, closure_1_end)) %>%
     mutate(across(c(closure_1_end, closure_2_end, closure_2), ~as.Date(.)))
 
+  ## getting only the columns I want
+  weekly_crime <- weekly_crime %>%
+    select(university, starts_with("closure_1"), treatment, alcohol_offense, sexual_assault,
+           robbery_burglary, theft, drug_offense, week, total_students_all)
 
-  event_study_df <- weekly_crime %>%
-    mutate(closure_1_start_week = floor_date(closure_1, unit = "week",
-                                             week_start = getOption('lubridate.week.start', 1)
-    )) %>%
-    mutate(closure_1_end_week = ceiling_date(closure_1_end - days(7), unit = "week",
-                                             week_start = getOption('lubridate.week.start', 1)
-    )) %>%
-    mutate(closure_2_start_week = floor_date(closure_2, unit = "week",
-                                             week_start = getOption('lubridate.week.start', 1)
-    )) %>%
-    mutate(closure_2_end_week = floor_date(closure_2_end, unit = "week",
-                                           week_start = getOption('lubridate.week.start', 1)
-    ))  %>%
-    mutate(treatment_1 = ifelse(week >= closure_1_start_week & week < closure_1_end_week, 1, 0)) %>%
-    mutate(treatment_2 = ifelse(week >= closure_2_start_week & week < closure_2_end_week, 1, 0))
+  ## getting two closure schools
+  ## rename the columns so that everything is standardized by closure_1 and closure_1_end
+  two_closure_schools <- data %>%
+    select(university, closure_2, closure_2_end, treatment, alcohol_offense, sexual_assault,
+           robbery_burglary, theft, drug_offense,  week, total_students_all) %>%
+    mutate(university = ifelse(!is.na(closure_2),paste0(university, "_2"), university)) %>%
+    rename(closure_1 = closure_2, closure_1_end = closure_2_end) %>%
+    filter(str_detect(university, "_2$")) %>%
+    filter(university != "Louisiana State University and Agricultural & Mechanical College_2")
 
-  two_treatment_uni <- event_study_df %>%
-    filter(!is.na(closure_2)) %>%
-    mutate(treatment_1 = treatment_2) %>%
-    mutate(closure_1 = closure_2) %>%
-    mutate(closure_1_end = closure_2_end) %>%
-    mutate(closure_1_end_week = closure_2_end_week) %>%
-    mutate(closure_1_start_week = closure_2_start_week) %>%
-    filter(university != "Louisiana State University and Agricultural & Mechanical College") %>% ## gets rid of LSU from 2nd
-    mutate(university = paste(university,"_2", sep= ""))
+  ## append the two together
+  event_study <- bind_rows(weekly_crime, two_closure_schools)
 
-  event_study <- bind_rows(event_study_df, two_treatment_uni)
+  event_study <- event_study %>%
+    mutate(week_id = wday(week, label = T)) %>%
+    mutate(closure_1_floor = floor_date(closure_1, unit = "week",
+                                        week_start = getOption('lubridate.week.start', 1)
+    )) %>%
+    mutate(closure_1_ceiling = ceiling_date(closure_1_end, unit = "week",
+                                            week_start = getOption('lubridate.week.start', 1)))
+  event_study <- event_study %>%
+    mutate(treatment = ifelse(week >= closure_1_floor & week < closure_1_ceiling,1, 0))
 
   for (i in window) {
     colname <- paste("treatment_plus_", i, sep = "")
     event_study <- event_study %>%
-      mutate(!!sym(colname) := (closure_1_end_week - days(7)) + weeks(i))
+      mutate(!!sym(colname) := (closure_1_ceiling - weeks(1)) + weeks(i))
   }
 
-  ## creating new columns called "closure_minus_" for the event window
   for (i in window) {
     colname <- paste("treatment_minus_", i, sep = "")
     event_study <- event_study %>%
-      mutate(!!sym(colname) := closure_1_start_week -  weeks(i))
+      mutate(!!sym(colname) := closure_1_floor -  weeks(i))
   }
-
-  ## treatment_1 is my main treatment variable I am working with - getting rid of these
-  ## to make it less confusing on myself
-  event_study <- event_study %>%
-    select(-treatment, -starts_with("treatment_2_"), -starts_with("treatment_1_"))
-
 
   event_study <- event_study %>%
     mutate(across(c(starts_with("treatment_minus"), starts_with("treatment_plus"),
     ), ~ifelse(. == week, 1, 0)))
 
-  event_study <- event_study %>%
-    mutate(treatment_minus_1 = 0)
+
 
   colname_minus <- paste("treatment_minus_", window_length, sep = "")
   colname_plus <- paste("treatment_plus_", window_length, sep = "")
 
-  ## this will bin up the end points
+
   event_study <- event_study %>%
     mutate(!!sym(colname_minus) := case_when(
-      closure_1_start_week - weeks(window_length) >= week & !is.na(!!sym(colname_minus))~1,
-      closure_1_start_week - weeks(window_length) <= week & !is.na(!!sym(colname_minus)) ~0)) %>%
+      closure_1_floor - weeks(window_length) >= week & !is.na(!!sym(colname_minus))~1,
+      closure_1_floor - weeks(window_length) <= week & !is.na(!!sym(colname_minus)) ~0)) %>%
     mutate(!!sym(colname_plus) := case_when(
-      (closure_1_end_week) + weeks(window_length- 1) <= week & !is.na(!!sym(colname_plus))~1,
-      (closure_1_end_week) + weeks(window_length - 1) >= week & !is.na(!!sym(colname_plus)) ~0
-    )) %>%
-    rename("treatment" = treatment_1) %>%
-    select(-treatment_2)
+      (closure_1_ceiling) + weeks(window_length- 1) <= week & !is.na(!!sym(colname_plus))~1,
+      (closure_1_ceiling) + weeks(window_length - 1) >= week & !is.na(!!sym(colname_plus)) ~0
+    ))
+
+  reference_week <- paste0("treatment_minus_", reference_week)
+
+  event_study <- event_study %>%
+    mutate(!!sym(reference_week) := 0)
+
   return(event_study)
 }
