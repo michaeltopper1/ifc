7941c354a45a142e9812693313d637bda50824bc
miketopper123 <miketopper123@gmail.com>
2021-04-19 23:27:12 -0700

    i don't even know anymore

--- R/event_study_stagger.R
+++ R/event_study_stagger.R
@@ -1,8 +1,7 @@
-
 #' Creates the Event Study tibble for analysis at Daily Level Data.
 #'
 #' @description
-#' `event_study_day` returns a tibble ready for event study analysis in units of days
+#' `event_study_stagger` returns a tibble ready for event study analysis in units of days with staggered adoption
 #'
 #' @details
 #' This function creates the event study tibble needed to do analysis in event study framework.
@@ -14,21 +13,19 @@
 #' @param cluster_blocks a number for how many days you want clustered together. I usually do 7 for week.
 #'
 #' @examples
-#' event_study_day(daily_crime,  window = 8, cluster_blocks = 7, reference_days =1 )
+#' event_study_stagger(daily_crime,  window = 8, cluster_blocks = 7, reference_days =1 )
 #'
-#' @export event_study_day
+#' @export event_study_stagger
 #'
-
-
-
-event_study_day <- function(data, window, cluster_blocks, reference_day) {
+#'
+event_study_stagger_day<- function(data, window, cluster_blocks, reference_day) {
   window <- c(1:window)
   event_study <- data %>%
     mutate(across(c(closure_1_end, closure_2_end), ~ as.character(.))) %>%
     mutate(closure_1_end = ifelse(university == "Louisiana State University and Agricultural & Mechanical College",
                                   closure_2_end, closure_1_end)) %>%
     mutate(across(c(closure_1_end, closure_2_end, closure_2), ~as.Date(.))) %>%
-    mutate(treatment = ifelse(date >= closure_1 & date < closure_1_end, 1, 0)) %>%
+    mutate(treatment = ifelse(date >= closure_1, 1, 0)) %>%
     select(-starts_with("closure_2"))
 
 
@@ -37,8 +34,8 @@ event_study_day <- function(data, window, cluster_blocks, reference_day) {
     closure_plus_end <- paste0("closure_plus_",i, "_end")
     colname_treat <- paste0("treatment_plus_", i)
     event_study <- event_study %>%
-      mutate(!!sym(closure_plus) := (closure_1_end + (i-1) *days(cluster_blocks))) %>%
-      mutate(!!sym(closure_plus_end) := closure_1_end + ((i) * days(cluster_blocks))) %>%
+      mutate(!!sym(closure_plus) := (closure_1 + (i) *days(cluster_blocks))) %>%
+      mutate(!!sym(closure_plus_end) := closure_1 + ((i+1) * days(cluster_blocks))) %>%
       mutate(!!sym(colname_treat) := ifelse(date >= !!sym(closure_plus) & date < !!sym(closure_plus_end), 1, 0))
   }
 
@@ -64,5 +61,8 @@ event_study_day <- function(data, window, cluster_blocks, reference_day) {
   event_study <- event_study %>%
     mutate(!!sym(endpoint_minus) := ifelse(date < !!sym(final_closure_minus), 1, 0 )) %>%
     mutate(!!sym(endpoint_plus) := ifelse(date >= !!sym(final_closure_plus), 1, 0))
+  event_study <- event_study %>%
+    mutate(treatment = ifelse(date >= closure_1 & date < (closure_1 + days(7)), 1, 0))
+
   return(event_study)
 }
