06229766db8df5e2f8a6deecbd39e70cf6002dca
miketopper123 <miketopper123@gmail.com>
2021-04-30 15:46:14 -0700

    adding in two functions for lists of schools

---
+++ R/death_universities.R
@@ -0,0 +1,43 @@
+#' Returns list of universities which underwent a fraternity death in 2014-2019.
+#'
+#' @description
+#' `death_universities` returns a list of universities in which a moratorium has occurred due to death in addition to never-treated schools.
+#'
+#' @details
+#' This function simply returns a list of universities. Does not rely on anything.
+#'
+#' @examples
+#' death_universities()
+#'
+#' @export death_universities
+#'
+
+death_universities <- function() {
+  death_universities <- c("Florida State University",
+                          "Louisiana State University and Agricultural & Mechanical College",
+                          "Murray State University",
+                          "Texas State University",
+                          "San Diego State University",
+                          "University at Buffalo",
+                          "University of Iowa",
+                          "University of Vermont",
+                          "Washington State University",
+                          "West Virginia University",
+                          "University of Kentucky",
+                          "Hampden-Sydney College",
+                          "University of Nevada-Reno",
+                          "University of Nebraska-Lincoln",
+                          "Texas Christian University",
+                          "Texas Tech University",
+                          "University of South Carolina-Columbia",
+                          "Ferrum College",
+                          "Texas A & M University-College Station",
+                          "University of California-Riverside",
+                          "Delaware State University",
+                          "University of California-Irvine",
+                          "California State University-Fullerton",
+                          "SUNY at Albany")
+  return(death_universities)
+}
+death_universities()
+
---
+++ R/untreated_universities.R
@@ -0,0 +1,32 @@
+#' Returns list of universities which underwent a fraternity death in 2014-2019 but no moratorium.
+#'
+#' @description
+#' `untreated_universities` returns a list of universities in which a moratorium has occurred due to death in addition to never-treated schools.
+#'
+#' @details
+#' This function simply returns a list of schools.
+#'
+#' @examples
+#' death_universities()
+#'
+#' @export untreated_universities
+#'
+
+untreated_universities <- function() {
+  untreated_universities <- c("University of Kentucky",
+                              "Hampden-Sydney College",
+                              "University of Nevada-Reno",
+                              "University of Nebraska-Lincoln",
+                              "Texas Christian University",
+                              "Texas Tech University",
+                              "University of South Carolina-Columbia",
+                              "Ferrum College",
+                              "Texas A & M University-College Station",
+                              "University of California-Riverside",
+                              "Delaware State University",
+                              "University of California-Irvine",
+                              "California State University-Fullerton",
+                              "SUNY at Albany")
+  return(untreated_universities)
+}
+
