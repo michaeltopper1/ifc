dcbaef24d8b04146d55a099353cd2b07426afeb9
Michael Topper <miketopper123@gmail.com>
2021-12-02 13:15:59 -0800

    new event study graph function

---
+++ R/event_study_graph.R
@@ -0,0 +1,40 @@
+#' Graphs an event study with no labs.
+#'
+#' @description
+#' `event_study_graph` returns a ggplot object of the event study graph.
+#'
+#' @details
+#' This function relies on dplyr, ggplot, and tibble packages to work.
+#'
+#' @param x a regression fixest object.
+#' @param window_size the length of the event window.
+#' @param reference_period the period to be omitted, defaults to period before. This is found by counting from last period to period wanted.
+#'
+#' @examples
+#' event_study_graph(x, 8, 8)
+#'
+#' @export event_study_graph
+#'
+
+
+
+event_study_graph <- function(x, window_size, reference_period = window_size) {
+  coefs <- broom::tidy(x, conf.int =T)
+  window_size_times_two <- window_size *2
+  coefs <- coefs %>%
+    add_row(.before = reference_period) %>%
+    mutate(time = c(-window_size:window_size)) %>%
+    mutate(across(.cols = -c(term), ~ ifelse(is.na(.), 0, .))) %>%
+    slice(2:window_size_times_two)
+  plot <- coefs %>%
+    ggplot(aes(x = time, y = estimate), alpha = 0.8) +
+    geom_ribbon(aes(ymin = conf.low, ymax = conf.high),fill = "light grey", alpha =0.8) +
+    geom_path(linetype = "dashed", color = "black") +
+    geom_point() +
+    # geom_errorbar(aes(x = value, ymin = conf.low, ymax = conf.high), alpha = 0.8) +
+    # geom_rect(aes(xmin = -0.5, xmax =0.5, ymin = -Inf, ymax = Inf), fill = "light blue", alpha = 0.03) +
+    scale_x_continuous(labels = c(-window_size:window_size), breaks = c(-window_size:window_size)) +
+    theme_minimal() +
+    geom_hline(yintercept = 0, color = "dark red")
+  return(plot)
+}
