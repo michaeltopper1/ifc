1030f35645a0cb9c2c6c961b4c8058cfef2beac6
Michael Topper <miketopper123@gmail.com>
2021-12-02 14:52:09 -0800

    new event study functions

--- R/event_study_day.R
+++ R/event_study_day.R
@@ -1,68 +1,121 @@
-
 #' Creates the Event Study tibble for analysis at Daily Level Data.
 #'
 #' @description
-#' `event_study_day` returns a tibble ready for event study analysis in units of days
+#' `event_study_day` returns a tibble ready for event study analysis in units of days (no staggers)
 #'
 #' @details
 #' This function creates the event study tibble needed to do analysis in event study framework.
 #'
 #'
-#' @param data an object/dataframe/tibble.
-#' @param window how big the event window is (in weeks)
-#' @param reference_days a number for which year you want to be the reference week It will be paste0("treatment_minus_", reference_year)
-#' @param cluster_blocks a number for how many days you want clustered together. I usually do 7 for week.
+#' @param data an object/dataframe/tibble (daily_crime).
+#' @param leads_lags how big the event window is.
+#' @param clusters a number for how many days you want clustered together.
 #'
 #' @examples
-#' event_study_day(daily_crime,  window = 8, cluster_blocks = 7, reference_days =1 )
+#' event_study_day(daily_crime, leads_lags = 8, bin_length =7)
 #'
 #' @export event_study_day
 #'
+#'
 
 
-
-event_study_day <- function(data, window, cluster_blocks, reference_day) {
-  window <- c(1:window)
-  event_study <- data %>%
-    mutate(across(c(closure_1_end, closure_2_end), ~ as.character(.))) %>%
-    mutate(closure_1_end = ifelse(university == "Louisiana State University and Agricultural & Mechanical College",
-                                  closure_2_end, closure_1_end)) %>%
-    mutate(across(c(closure_1_end, closure_2_end, closure_2), ~as.Date(.))) %>%
-    mutate(treatment = ifelse(date >= closure_1 & date < closure_1_end, 1, 0)) %>%
-    select(-starts_with("closure_2"))
-
-
-  for (i in window) {
-    closure_plus <- paste("closure_plus_", i, sep = "")
-    closure_plus_end <- paste0("closure_plus_",i, "_end")
-    colname_treat <- paste0("treatment_plus_", i)
-    event_study <- event_study %>%
-      mutate(!!sym(closure_plus) := (closure_1_end + (i-1) *days(cluster_blocks))) %>%
-      mutate(!!sym(closure_plus_end) := closure_1_end + ((i) * days(cluster_blocks))) %>%
-      mutate(!!sym(colname_treat) := ifelse(date >= !!sym(closure_plus) & date < !!sym(closure_plus_end), 1, 0))
+event_study_day <- function(data, leads_lags, clusters) {
+  leead <- function(x, v){
+    xx <- rep(0, length(x))
+    for(i in v){
+      xx <- xx + lead(x, i)
+    }
+    xx[is.na(xx)] <- 0
+    xx
   }
 
-  for (i in window) {
-    closure_minus <- paste("closure_minus_", i, sep = "")
-    closure_minus_end <- paste0("closure_minus_",i, "_end")
-    colname_treat <- paste0("treatment_minus_", i)
-    event_study <- event_study %>%
-      mutate(!!sym(closure_minus) := closure_1 -  i *days(cluster_blocks)) %>%
-      mutate(!!sym(closure_minus_end) := closure_1 - ((i- 1) * days(cluster_blocks))) %>%
-      mutate(!!sym(colname_treat) := ifelse(date >= !!sym(closure_minus) & date < !!sym(closure_minus_end), 1, 0))
+  laag <- function(x, v){
+    xx <- rep(0, length(x))
+    for(i in v){
+      xx <- xx + dplyr::lag(x, i)
+    }
+    xx[is.na(xx)] <- 0
+    xx
   }
 
-  reference_days <- paste0("treatment_minus_", reference_day)
-  event_study <- event_study %>%
-    mutate(!!sym(reference_days) := 0)
+  es <- data %>%
+    group_by(university) %>%
+    arrange(date) %>%
+    mutate(first_day_moratorium = case_when(
+      closure_1 == date ~ 1,
+      closure_2 == date ~ 1,
+      closure_3 == date ~ 1,
+      university == "Florida International University" & date == "2018-01-04" ~ 1,
+      TRUE ~as.double(0))) %>%
+    mutate(last_day_moratorium = case_when(
+      closure_1_end == date + lubridate::days(1) ~ 1,
+      closure_2_end == date +lubridate::days(1)~ 1,
+      closure_3_end == date + lubridate::days(1)~1,
+      university == "San Diego State University" & date == "2014-12-16" ~1,
+      university == "University of Virginia-Main Campus" & date == "2014-12-18" ~ 1,
+      university == "Washington State University" & date == "2016-12-22" ~1,
+      university == "Monmouth University" & date == "2018-12-23" ~1,
+      TRUE ~ as.double(0)
+    ))
 
-  endpoint_plus <- paste0("treatment_plus_", length(window))
-  endpoint_minus <- paste0("treatment_minus_", length(window))
-  final_closure_minus <- paste0("closure_minus_", length(window) - 1)
-  final_closure_plus <- paste0("closure_plus_", length(window))
-  event_study %>% colnames()
-  event_study <- event_study %>%
-    mutate(!!sym(endpoint_minus) := ifelse(date < !!sym(final_closure_minus), 1, 0 )) %>%
-    mutate(!!sym(endpoint_plus) := ifelse(date >= !!sym(final_closure_plus), 1, 0))
-  return(event_study)
+
+  leads <- leads_lags
+  lags <- leads_lags
+  bin_length <- clusters
+
+  starts <- seq(from = 1, to = leads*bin_length, by = bin_length)
+
+  lead_counts <- seq(from = bin_length, to = leads *bin_length, by = bin_length)
+  lag_counts <- seq(from = bin_length, to = lags*bin_length, by = bin_length)
+
+  for (i in 1:leads){
+    name <- paste0("beta_lead_", i)
+    es <- es %>%
+      mutate(!!sym(name) := leead(first_day_moratorium, c(starts[i]:lead_counts[i])))
+
+  }
+
+
+  for (i in 1:lags) {
+    name <- paste0("beta_lag_", i)
+    es <- es %>%
+      mutate(!!sym(name) := laag(last_day_moratorium, c(starts[i]:lag_counts[i])))
+  }
+
+  ## binning endpoints cumulatively
+  last_lag <- paste0("beta_lag_", lags)
+  es <- es %>%
+    relocate(starts_with("beta_lag_"), treatment, date, university) %>%
+    ungroup() %>%
+    group_by(university) %>%
+    arrange(date) %>%
+    mutate(beta_lag_binned = cumsum(!!sym(last_lag))) %>%
+    relocate(beta_lag_binned) %>%
+    ungroup()
+
+
+  ## binning endpoints cumulatively
+  first_lead <- paste0("beta_lead_", leads)
+  es <- es %>%
+    relocate(starts_with("beta_lead_"), treatment, date, university) %>%
+    ungroup() %>%
+    group_by(university) %>%
+    arrange(desc(date)) %>%
+    mutate(beta_lead_binned = cumsum(!!sym(first_lead))) %>%
+    relocate(beta_lead_binned) %>%
+    ungroup()
+
+  ## changing end points
+  es <- es %>%
+    group_by(university) %>%
+    arrange(date) %>%
+    mutate(across(c(beta_lead_binned, beta_lag_binned), ~case_when(
+      . >= 1 & . <= bin_length ~1,
+      . > bin_length & . <=bin_length *2 ~ 2,
+      . > bin_length*2 ~3,
+      TRUE ~as.double(0))
+    )) %>%
+    ungroup()
+
+  return(es)
 }
--- R/event_study_stagger.R
+++ R/event_study_stagger.R
@@ -7,62 +7,124 @@
 #' This function creates the event study tibble needed to do analysis in event study framework.
 #'
 #'
-#' @param data an object/dataframe/tibble.
-#' @param window how big the event window is (in weeks)
-#' @param reference_days a number for which year you want to be the reference week It will be paste0("treatment_minus_", reference_year)
-#' @param cluster_blocks a number for how many days you want clustered together. I usually do 7 for week.
+#' @param data an object/dataframe/tibble (daily_crime).
+#' @param leads_lags how big the event window is.
+#' @param clusters a number for how many days you want clustered together.
 #'
 #' @examples
-#' event_study_stagger(daily_crime,  window = 8, cluster_blocks = 7, reference_days =1 )
+#' event_study_stagger(daily_crime, leads_lags = 8, bin_length =7)
 #'
 #' @export event_study_stagger
 #'
 #'
-event_study_stagger<- function(data, window, cluster_blocks, reference_day) {
-  window <- c(1:window)
-  event_study <- data %>%
-    mutate(across(c(closure_1_end, closure_2_end), ~ as.character(.))) %>%
-    mutate(closure_1_end = ifelse(university == "Louisiana State University and Agricultural & Mechanical College",
-                                  closure_2_end, closure_1_end)) %>%
-    mutate(across(c(closure_1_end, closure_2_end, closure_2), ~as.Date(.))) %>%
-    mutate(treatment = ifelse(date >= closure_1, 1, 0)) %>%
-    select(-starts_with("closure_2"))
-
-
-  for (i in window) {
-    closure_plus <- paste("closure_plus_", i, sep = "")
-    closure_plus_end <- paste0("closure_plus_",i, "_end")
-    colname_treat <- paste0("treatment_plus_", i)
-    event_study <- event_study %>%
-      mutate(!!sym(closure_plus) := (closure_1 + (i) *days(cluster_blocks))) %>%
-      mutate(!!sym(closure_plus_end) := closure_1 + ((i+1) * days(cluster_blocks))) %>%
-      mutate(!!sym(colname_treat) := ifelse(date >= !!sym(closure_plus) & date < !!sym(closure_plus_end), 1, 0))
+event_study_stagger<- function(data, leads_lags, clusters) {
+  leead <- function(x, v){
+    xx <- rep(0, length(x))
+    for(i in v){
+      xx <- xx + lead(x, i)
+    }
+    xx[is.na(xx)] <- 0
+    xx
   }
 
-  for (i in window) {
-    closure_minus <- paste("closure_minus_", i, sep = "")
-    closure_minus_end <- paste0("closure_minus_",i, "_end")
-    colname_treat <- paste0("treatment_minus_", i)
-    event_study <- event_study %>%
-      mutate(!!sym(closure_minus) := closure_1 -  i *days(cluster_blocks)) %>%
-      mutate(!!sym(closure_minus_end) := closure_1 - ((i- 1) * days(cluster_blocks))) %>%
-      mutate(!!sym(colname_treat) := ifelse(date >= !!sym(closure_minus) & date < !!sym(closure_minus_end), 1, 0))
+  laag <- function(x, v){
+    xx <- rep(0, length(x))
+    for(i in v){
+      xx <- xx + dplyr::lag(x, i)
+    }
+    xx[is.na(xx)] <- 0
+    xx
   }
 
-  reference_days <- paste0("treatment_minus_", reference_day)
-  event_study <- event_study %>%
-    mutate(!!sym(reference_days) := 0)
+  ## Had to change florida's date since their closure date was outside of their academic calendar I have.
+  es <- data %>%
+    group_by(university) %>%
+    arrange(date) %>%
+    mutate(first_day_moratorium = case_when(
+      closure_1 == date ~ 1,
+      closure_2 == date ~ 1,
+      closure_3 == date ~ 1,
+      university == "Florida International University" & date == "2018-01-04" ~ 1,
+      TRUE ~as.double(0)))
 
-  endpoint_plus <- paste0("treatment_plus_", length(window))
-  endpoint_minus <- paste0("treatment_minus_", length(window))
-  final_closure_minus <- paste0("closure_minus_", length(window) - 1)
-  final_closure_plus <- paste0("closure_plus_", length(window))
-  event_study %>% colnames()
-  event_study <- event_study %>%
-    mutate(!!sym(endpoint_minus) := ifelse(date < !!sym(final_closure_minus), 1, 0 )) %>%
-    mutate(!!sym(endpoint_plus) := ifelse(date >= !!sym(final_closure_plus), 1, 0))
-  event_study <- event_study %>%
-    mutate(treatment = ifelse(date >= closure_1 & date < (closure_1 + days(7)), 1, 0))
 
-  return(event_study)
+  leads <- leads_lags
+  lags <- leads + 1 ## since we need to put in a treatment variable
+
+  ## determining how many days you want to be in each lag and lead bin
+  bin_length <- clusters
+
+
+  starts <- seq(from = 1, to = leads*bin_length, by = bin_length)
+
+  lead_counts <- seq(from = bin_length, to = leads *bin_length, by = bin_length)
+
+
+  ## to accomodate the fact that I need to start with my "beta_0", I needed to add to the length of these
+  ## next vectors so that my 5 lags would in fact be 1 treatment "beta_0" and 5 lags.
+  ## moreover, I needed to substract by 1 since i wanted the laag function to start at the first day of moratorium
+  starts_lag <- seq(from = 1, to = (lags +1)*bin_length, by = bin_length) - 1
+  lag_counts <- seq(from = bin_length, to = (lags + 1)*bin_length, by = bin_length) - 1
+
+  for (i in 1:leads){
+    name <- paste0("beta_lead_", i)
+    es <- es %>%
+      mutate(!!sym(name) := leead(first_day_moratorium, c(starts[i]:lead_counts[i])))
+
+  }
+
+
+  for (i in 1:lags) {
+    if (i == 1) {
+      name <- "beta_0"
+      es <- es %>%
+        mutate(!!sym(name) := laag(first_day_moratorium, c(starts_lag[i]:lag_counts[i])))
+    }
+    else{
+      name <- paste0("beta_lag_", i -1)
+      es <- es %>%
+        mutate(!!sym(name) := laag(first_day_moratorium, c(starts_lag[i]:lag_counts[i])))
+    }
+  }
+
+  ## binning endpoints cumulatively
+  last_lag <- paste0("beta_lag_", lags -1)
+  es <- es %>%
+    relocate(starts_with("beta_lag_"), treatment, date, university) %>%
+    ungroup() %>%
+    group_by(university) %>%
+    arrange(date) %>%
+    mutate(beta_lag_binned = cumsum(!!sym(last_lag))) %>%
+    relocate(beta_lag_binned) %>%
+    ungroup()
+
+
+  ## binning endpoints cumulatively
+  first_lead <- paste0("beta_lead_", leads)
+  es <- es %>%
+    relocate(starts_with("beta_lead_"), treatment, date, university) %>%
+    ungroup() %>%
+    group_by(university) %>%
+    arrange(desc(date)) %>%
+    mutate(beta_lead_binned = cumsum(!!sym(first_lead))) %>%
+    relocate(beta_lead_binned) %>%
+    ungroup()
+
+  ## changing end points
+  es <- es %>%
+    group_by(university) %>%
+    arrange(date) %>%
+    mutate(across(c(beta_lead_binned, beta_lag_binned), ~case_when(
+      . >= 1 & . <= bin_length ~1,
+      . > bin_length & . <=bin_length *2 ~ 2,
+      . > bin_length*2 ~3,
+      TRUE ~as.double(0))
+    )) %>%
+    # mutate(across(c(beta_lead_binned, beta_lag_binned), ~case_when(
+    #   . >= 1 ~1,
+    #   TRUE ~as.double(0))
+    # )) %>%
+    ungroup()
+
+  return(es)
 }
